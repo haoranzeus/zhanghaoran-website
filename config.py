158cfd8b39e7fcf57be7ed8f3ab1c75af90374c4
zhanghaoran <zhanghaoran@ACA84268.ipt.aol.com>
2016-04-03 17:06:21 +0800

    add config file

---
+++ config.py
@@ -0,0 +1,34 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    
+
+class TestingConfig(Cofig):
+    TESTING = True
+
+
+class ProductionConfig(Config):
+    pass
+
+
+config = {
+        'development': DevelopmentConfig,
+        'testing': TestingConfig,
+        'production': ProductionConfig,
+
+        'default': DevelopmentConfig,
+        }
+
+
