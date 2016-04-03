4843033f3db52eb92fb08f062cab281ea7217395
zhanghaoran <zhanghaoran@ACA84268.ipt.aol.com>
2016-04-03 17:38:04 +0800

    start to write app/__init__.py

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.mail import Mail
+from flask.ext.moment import Moment
+from flask.ext.sqlalchemy import SQLAlchemy
+from config import config
+
+
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    pass
+
